6fce21687418aea8da872ff6b652f8bf7bd9b324
Carlos Matos <chcmatos@hotmail.com>
2023-04-07 03:15:08 -0300

    packages updated.

---
+++ Atomatus.Bootstarter.Web/Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+                
+
+        }
+    }
+}
