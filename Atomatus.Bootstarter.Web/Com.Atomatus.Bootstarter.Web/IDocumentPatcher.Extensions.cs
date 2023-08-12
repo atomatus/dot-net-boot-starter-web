a39496b1274f0698a4b7a67da2be9adc7508e020
Carlos Matos <chcmatos@hotmail.com>
2023-08-12 16:17:14 -0300

    IDocumentPatcher was implemented.

---
+++ Atomatus.Bootstarter.Web/Com.Atomatus.Bootstarter.Web/IDocumentPatcher.Extensions.cs
@@ -0,0 +1,58 @@
+﻿using Com.Atomatus.Bootstarter.Model;
+using Com.Atomatus.Bootstarter.Util;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace Com.Atomatus.Bootstarter.Web
+{
+    /// <summary>
+    /// <see cref="IDocumentPatcher"/> extension methods.
+    /// </summary>
+    public static class IDocumentPatcherExtensions
+    {
+        /// <summary>
+        /// <para>
+        /// Copy all non null data from <paramref name="self"/> (IDocumentPatcher object) to <paramref name="target"/> object
+        /// that are sharing all same Properties name.
+        /// </para>
+        /// <para>
+        /// Property type must be compatible for one of follwing conditions: 
+        /// Same type, 
+        /// Derived type, 
+        /// Derived interfaces or 
+        /// Types with same Properties definition.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="self"></param>
+        /// <param name="target"></param>
+        public static void ApplyTo<T>(this IDocumentPatcher self, [NotNull] T target) where T: class, IModel
+        {
+            var sourceProperties = self.GetType().GetProperties().Where(p => p.CanRead);
+            var targetProperties = target.GetType().GetProperties();
+            foreach (var sourceProperty in sourceProperties)
+            {
+                var value = sourceProperty.GetValue(self, null);
+                if(value != null && targetProperties.FirstOrDefault(targetProperty => targetProperty.CanWrite && 
+                        sourceProperty.Name.Equals(targetProperty.Name, StringComparison.InvariantCulture)) is PropertyInfo found)
+                {
+                    try
+                    {
+                        var targetValue = ObjectMapper.Parse(value, found.PropertyType);
+                        found.SetValue(target, targetValue, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Write(ex, nameof(IDocumentPatcher));
+#if DEBUG
+                        throw;
+#endif
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Atomatus.Bootstarter.Web/Com.Atomatus.Bootstarter.Web/IDocumentPatcher.cs
@@ -0,0 +1,14 @@
+﻿namespace Com.Atomatus.Bootstarter.Web
+{
+    /// <summary>
+    /// <para>
+    /// Use this contract to enable usage of ApplyTo method to copy shared properties
+    /// by name and type between the attached object and another one.
+    /// </para>
+    /// <para>
+    /// Only the non-null property value will be copied to target object.
+    /// Therefore this contract class is usable for REST PATCH method where the object is partially changed.
+    /// </para>
+    /// </summary>
+    public interface IDocumentPatcher { }
+}
